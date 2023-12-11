9131393e617c5a59cfd52c46de02c55a16f095f5
Alex-Nav <alejugger@gmail.com>
2023-12-11 00:03:49 -0600

    shift and unshift

---
+++ JSPage2/dom.js
@@ -0,0 +1,4 @@
+import { dishes } from './functions.js';
+
+console.log(dishes('canel'));
+console.log(dishes('Tacoche'));
---
+++ JSPage2/functions.js
@@ -0,0 +1,10 @@
+
+// Exporta la función para que pueda ser importada en otro archivo
+export function dishes(nombrePlatillo) {
+    let platillos = ['Spaghetti', 'Burger', 'Sushi', 'Ramen', 'Pizza'];
+    if (platillos.length >= 5) {
+        platillos.unshift();
+    }
+    platillos.push(nombrePlatillo);
+    return platillos;
+}
