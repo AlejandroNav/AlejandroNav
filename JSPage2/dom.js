8bfdedde7464189c06d9ef92625a72411ad603fb
Alex-Nav <alejugger@gmail.com>
2023-12-11 00:14:44 -0600

    finished arrays

--- JSPage2/dom.js
+++ JSPage2/dom.js
@@ -2,3 +2,16 @@ import { dishes } from './functions.js';
 
 console.log(dishes('canel'));
 console.log(dishes('Tacoche'));
+
+
+let amigo = [1,2,3,7,8];
+(amigo.unshift(44));
+console.log(amigo);
+(amigo.shift(1));
+console.log(amigo);
+
+const userReview ={}
+userReview['Star Wars'] = 7.0;
+userReview['Star Trek'] = 5.0;
+userReview.amigos = 3.0
+console.log(userReview);
--- JSPage2/functions.js
+++ JSPage2/functions.js
@@ -8,3 +8,4 @@ export function dishes(nombrePlatillo) {
     platillos.push(nombrePlatillo);
     return platillos;
 }
+
