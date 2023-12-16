d5d9f054da978fad4874ef42d6e969e8559ac4a7
Alex-Nav <alejugger@gmail.com>
2023-12-15 23:18:27 -0600

    ca

--- JSPage2/dom.js
+++ JSPage2/dom.js
@@ -1,17 +1,18 @@
-import { dishes } from './functions.js';
+import { dishes } from './functions.js'; //imports a function named dishes from a file called functions.js in the same directory.
 
 console.log(dishes('canel'));
 console.log(dishes('Tacoche'));
 
 
 let amigo = [1,2,3,7,8];
-(amigo.unshift(44));
+(amigo.unshift(44)); //Adds 44 to the beginning of the amigo array. Now amigo becomes [44, 1, 2, 3, 7, 8].
 console.log(amigo);
-(amigo.shift(1));
+(amigo.shift());//Removes the first element from amigo. Now amigo becomes [1, 2, 3, 7, 8].
 console.log(amigo);
 
-const userReview ={}
-userReview['Star Wars'] = 7.0;
-userReview['Star Trek'] = 5.0;
+const userReview ={} //Declares an empty object userReview.
+userReview['Star Wars'] = 7.0; //Adds a property 'Star Wars' with the value 7.0 to userReview.
+userReview['Star Trek'] = 5.0;// bracket notation (object['property']) to add or modify a property 
+//Bracket notation is useful if property name is dynamic or not a valid JS, like spaces or special characters (space in 'Star Trek').
 userReview.amigos = 3.0
 console.log(userReview);
